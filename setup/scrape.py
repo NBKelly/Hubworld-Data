6dfec36e570c12796def801f7936ead96ec019ab
NB Kelly <N.B.Kelly@protonmail.com>
2025-03-22 19:25:27 +1300

    scraping done

--- setup/scrape.py
+++ setup/scrape.py
@@ -3,6 +3,7 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 
+import os
 import re
 import pprint
 
@@ -10,6 +11,12 @@ URL = "https://decksmith.app/hubworldaidalon/cards/"
 
 with open("setup/urls.data", "r") as urls:
     IDS = [line.rstrip() for line in urls]
+
+def slugify(s):
+    return re.sub(r'[^a-zA-Z]+', '-', s.lower().strip())
+
+def unslug(s):
+    return re.sub(r'-', ' ', s[1:]).capitalize()
 
 def get_title(soup, card):
     data = soup.find('h1', class_="text-3xl")
@@ -23,6 +30,8 @@ def get_title(soup, card):
     else:
         card[':uniqueness'] = False
     card[':title'] = title
+    card[':stripped-title'] = title
+    card[':id'] = slugify(title)
 
     if len(titles) == 2:
         subtitle = titles[1].get_text().strip()
@@ -40,9 +49,6 @@ numeric_keys = ["Action Limit", "Draw Limit", "Shard Limit", "Shard Cost", "Barr
 slugged_keys = ["Faction", "Type", "Collection Icons"]
 list_keys = ["Traits"]
 list_text_keys = ["Illustrator"]
-
-def slugify(s):
-    return re.sub(r'[^a-zA-Z]+', '-', s.lower().strip())
 
 def process_row(row, card):
     text = row.get_text().strip()
@@ -116,6 +122,118 @@ for ID in IDS:
 
 
     cards += [card]
-    #print(card)
 
-pprint.pp(cards)
+## now we have a set of all the cards... what can we do with that?
+## lets try and produce something that looks like edn format
+
+def format_key(v):
+    if v == ":traits":
+        return ":subtypes"
+    return v
+
+def format_value(v):
+    if isinstance(v, str):
+        if v.startswith(":"):
+            return v
+        return '"' + v + '"'
+    if str(v).isnumeric():
+        return str(v)
+    if v == True:
+        return "true"
+    if v == False:
+        return "nil"
+    # it's a list
+    return "[" + " ".join(format_value(val) for val in v) + "]"
+
+def format_card(card):
+    keys = sorted(card.keys())
+    lines = []
+    for key in keys:
+        lines += [format_key(key) + " " + format_value(card[key])]
+
+    return "{" + "\n ".join(lines) + "}"
+
+code = 1000
+position = 1
+
+def stripped_card(card):
+    c = dict(card)
+    c.pop(':illustrator', None)
+    c.pop(':url', None)
+    return c
+
+def format_set(card, code, position):
+    set_card = {':card-id': card[':id'],
+                ':code': (code + position),
+                ':position': position,
+                # TODO - are agents singleton? Is 3 the right number?
+                ':quantity': 3,
+                ':set-id': 'pre-release'}
+    return format_card(set_card)
+
+set_cards = []
+
+types = set()
+subtypes = set()
+
+for card in cards:
+    formatted_card = format_card(stripped_card(card))
+    f = open("edn/cards/" + card[':id'] + ".edn", "w")
+    f.write(formatted_card + "\n")
+    f.close()
+
+    position += 1
+    set_cards += [format_set(card, code, position)]
+
+#    print(card)
+    types.add(card[':type'])
+    for s in card[':traits']:
+        subtypes.add(s)
+
+    print("Downloading " + card[':url'] + "...")
+    os.system('wget -q -O img/' + str(code + position) + ".webp \"" + card[':url'] + '"')
+#    print(format_card(card))
+
+# write the set-cards file
+print("Writing set-cards/pre-release.edn...")
+f = open("edn/set-cards/pre-release.edn", "w")
+f.write("[\n " + "\n ".join(set_cards) + "]\n")
+f.close()
+
+# write the sets file
+print("Writing sets.edn...")
+sets = ['{:code "pre-release"\n  :cycle-id "pre-release"\n  :id "pre-release"\n  :name "Pre-Release"\n  :set-type :data-pack\n  :position 1\n  :size 25\n:data-release nil}']
+f = open("edn/sets.edn", "w")
+f.write("[" + "\n ".join(sets) + "]\n")
+f.close()
+
+# write the cycles file
+print("Writing cycles.edn...")
+cycles = ['{:id "pre-release"\n  :name "Pre-Release"\n  :position 0\n  :rotated false\n  :size 1}']
+f = open("edn/cycles.edn", "w")
+f.write("[" + "\n ".join(cycles) + "]\n")
+f.close()
+
+# write the formats file
+print("Writing formats.edn...")
+formats = ['{:id "pre-release"\n  :name "Pre-Release"\n  :sets ["pre-release"]\n  :mwl nil}']
+f = open("edn/formats.edn", "w")
+f.write("[" + "\n ".join(formats) + "]\n")
+f.close()
+
+# write the types file
+print("Writing types.edn...")
+types_str = "(" + "\n ".join("{:id " + type + "\n  :name " + unslug(type) + "}" for type in types) + ")\n"
+f = open("edn/types.edn", "w")
+f.write(types_str)
+f.close()
+
+# write the types file
+print("Writing subtypes.edn...")
+subtypes_str = "(" + "\n ".join("{:id " + type + "\n  :name \"" + unslug(type) + "\"}" for type in subtypes) + ")\n"
+f = open("edn/subtypes.edn", "w")
+f.write(subtypes_str)
+f.close()
+
+
+#    pprint.pp(card, width=300, sort_dicts=True)
